b29b9815a5341f0bb8cfe0b240cd4b6d2f470851
AdnanAlrashed <adnanalrashed77@gmail.com>
2025-09-30 23:18:45 +0300

    set

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a%64g-zi1csj#2jg=mif25p9)ls-$f$44y%)6#yuc!k=)3r!8&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
