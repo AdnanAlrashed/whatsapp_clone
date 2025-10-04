dbcbb00cfa4a4e248f3108768a135e9de2efe15f
AdnanAlrashed <adnanalrashed77@gmail.com>
2025-10-04 23:44:25 +0300

    add call

--- whatsapp_clone/asgi.py
+++ whatsapp_clone/asgi.py
@@ -7,14 +7,14 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsapp_clone.settings')
 django.setup()
 import chat.routing  # 👈 بعد تهيئة Django
-# import calls.routing  # 👈 بعد تهيئة Django
+import calls.routing  # 👈 بعد تهيئة Django
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns
-            # calls.routing.websocket_urlpatterns
+            chat.routing.websocket_urlpatterns +
+            calls.routing.websocket_urlpatterns
         )
     ),
 })
