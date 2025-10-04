4055722673dfb6b2b52114fb53c192ce7f376596
AdnanAlrashed <adnanalrashed77@gmail.com>
2025-10-04 23:02:13 +0300

    import

--- whatsapp_clone/asgi.py
+++ whatsapp_clone/asgi.py
@@ -1,11 +1,13 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing
-import calls.routing
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsapp_clone.settings')
+django.setup()
+import chat.routing  # 👈 بعد تهيئة Django
+import calls.routing  # 👈 بعد تهيئة Django
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
